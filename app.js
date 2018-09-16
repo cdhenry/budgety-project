0e4dc551c5cef8ba0c71c2c80a6eacba7560d15d
cdhenry <cdhenry@gmail.com>
2018-09-16 07:13:34 -0500

    feat: added event listener for button and enter keypress

--- app.js
+++ app.js
@@ -15,4 +15,28 @@ var UIController = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
     
+    var ctrlAddItem = function() {
+        
+        // 1. Get the field input data
+
+        // 2. add the item to the budget controller
+        
+        // 3. add the item to the ui controller
+        
+        // 4. calculatet he budget
+        
+        // 5. display the budget on the ui
+        
+        console.log('It Works')
+        
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            ctrlAddItem()          
+        }
+    })
+    
 })(budgetController, UIController)
