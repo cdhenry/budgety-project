8a88c578e2c99f6820ea5cee5ac28c4962579d45
cdhenry <cdhenry@gmail.com>
2018-09-16 07:12:15 -0500

    chore: added js file and architecture for controllers

---
+++ app.js
@@ -0,0 +1,18 @@
+//BUDGET CONTROLLER
+var budgetController = (function() {
+
+    
+    
+})()
+
+
+//UI CONTROLLER
+var UIController = (function(){
+    // Some Code
+})()
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+    
+})(budgetController, UIController)
